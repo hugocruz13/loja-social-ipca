6a1fde069aaf38e6c3a243d01a0503e9b0c5ac2e
Gustavo Pereira <a29852@alunos.ipca.pt>
2025-12-23 13:10:43 +0000

    Adição de Logo ao Iniciar APP

--- app/src/main/java/pt/ipca/lojasocial/presentation/main/MainActivity.kt
+++ app/src/main/java/pt/ipca/lojasocial/presentation/main/MainActivity.kt
@@ -8,11 +8,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import pt.ipca.lojasocial.presentation.ui.theme.LojaSocialIPCATheme
 import pt.ipca.lojasocial.presentation.navigation.AppNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
